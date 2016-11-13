10a4cf3a9f29838996fc5b7883322b54e3d65a92
Alex McVittie <alexander.mcvittie@gmail.com>
2016-11-13 16:49:45 -0500

    Working stuff

--- get_data.py
+++ get_data.py
@@ -2,6 +2,14 @@
 
 import sqlite3
 
+def clean_singleresult(result):
+    tmp = []
+    for row in result:
+        tmp.append(row)
+    tmp2 = []
+    for row in tmp:
+        tmp2.append(row[0])
+    return tmp2
 
 conn = sqlite3.connect("db.db")
 conn2 = sqlite3.connect("db.db")
@@ -9,9 +17,8 @@ c2 = conn2.cursor()
 c = conn.cursor()
 
 # Get prep time preferences from user
-time_pref = input("How long do you want to spend prepping (minutes)?:  ")
-cook_pref = input("How long do you want to spend cooking (Minutes)?:   ")
-
+#time_pref = input("How long do you want to spend prepping (minutes)?:  ")
+#cook_pref = input("How long do you want to spend cooking (Minutes)?:   ")
 
 
 a = c.execute("SELECT * FROM Fridge")
@@ -22,21 +29,50 @@ for row in a:
     fridge_table.append(row)
 
 
-b = c.execute("SELECT * FROM RecipeFile WHERE prepTime <= {} AND cookTime <= {}".format(time_pref, cook_pref))
+b = c.execute("SELECT recipeID, recipeName FROM RecipeFile")
 
-recipe_table = []
+recipe_table = {}
+
+a = []
+# Put the recipe ID's that fit time criteria into a 1D list
 for row in b:
-    recipe_table.append(row)
+    a.append(row)
+print("asdfjkdjfkdjfkjsdfjsd")
+print(a)
+for row in a:
+    recipe_table[row[0]] = row[1]
 
+print(recipe_table)
 
-print (fridge_table)
-print (recipe_table)
+for a_recipeID in recipe_table.keys():
+    result = c.execute("SELECT ingredientID, qty, qtyType FROM Ingredients WHERE recipeID =={}".format(a_recipeID))
+    ingredients_needed = []
+    for row in result:
+        ingredients_needed.append(row)
+    
+    can_make = True
+    for ingredient in ingredients_needed:
+        fridge_items = c.execute("SELECT ingredientID, qty, qtyType FROM Fridge WHERE ingredientID == {}".format(ingredient[0]))
+        exists = 0
+        for row in fridge_items:
+            exists += 1
+        if exists == 0:
+            #print("no food for you")
+            can_make = False
+            break
+        else:
+            pass
+            #print("got it!")
+    if can_make:
+        print("You can make {}".format(recipe_table[a_recipeID]))
+
 
 ingredients_i_have = []
 for indice in fridge_table:
     ingredients_i_have.append(indice[0])
 
+
 print(ingredients_i_have)
 
-print(fridge_table[1][1])
+#print(fridge_table[1][1])
 
